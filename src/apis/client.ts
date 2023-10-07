ad864c74133557f642f64cbaae184384d6f6a5e8
Long Vo <vodoanhoanglong10a9@gmail.com>
2023-10-07 22:55:00 +0700

    Change endpoint api

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, DefaultOptions, HttpLink, InMemoryCache } from "@apollo/c
 import { setContext } from "@apollo/client/link/context";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 
-const serverUrl = "https://190f-2405-4802-801c-e0-779-6954-d556-ddcd.ngrok-free.app/v1/graphql";
+const serverUrl = "https://7e95-2405-4802-801c-e0-ede3-98cb-5f83-7cc1.ngrok-free.app/v1/graphql";
 const guestToken = `u4g70QI}6\dSCELiOor9r&nZ:i25j]:3`;
 
 const graphqlUrl = new HttpLink({
