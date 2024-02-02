c65911c583ae6ec32216de50a0bb4828ed004c9c
vodoanhoanglong <vodoanhoanglong10a9@gmail.com>
2024-02-02 15:14:18 +0700

    Update package to gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import { GoogleTagManager } from "@next/third-parties/google";
 import "animate.css";
 import type { Metadata } from "next";
-import TagManager from "react-gtm-module";
 import "react-quill/dist/quill.snow.css";
 import "react-toastify/dist/ReactToastify.css";
 import "swiper/css";
@@ -18,18 +18,13 @@ export const metadata: Metadata = {
     "Tiến Hùng Chemicals ra đời với tầm nhìn là trở thành một đối tác đáng tin cậy và phát triển bền vững trong lĩnh vực hoá chất công nghiệp. Sự cam kết về chất lượng, sự sáng tạo và khả năng đáp ứng linh hoạt là những đặc điểm định danh của Hoá chất Tiến Hùng. Chúng tôi mong muốn xây dựng mối quan hệ đối tác bền vững với khách hàng và cộng đồng, đồng hành trong việc đạt được sự thành công và phát triển bền vững. Hãy để chúng tôi là người bạn đồng hành đáng tin cậy của bạn trong lĩnh vực hoá chất, và chúng tôi sẽ luôn cố gắng hết mình để đem lại giá trị tốt nhất cho bạn.",
 };
 
-const tagManagerArgs = {
-  gtmId: process.env.NEXT_PUBLIC_GTM,
-};
-
-if (process.browser) TagManager.initialize(tagManagerArgs);
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body suppressHydrationWarning>
         <main>{children}</main>
       </body>
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
     </html>
   );
 }
